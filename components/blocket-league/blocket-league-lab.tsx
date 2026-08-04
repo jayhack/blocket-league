1fe326195ecc21c5172103201b73104439effb54
Jay Hack <jayhack.0@gmail.com>
2026-08-04 15:42:19 -0700

    Add Modal conclusion credits

--- components/blocket-league/blocket-league-lab.tsx
+++ components/blocket-league/blocket-league-lab.tsx
@@ -679,6 +679,15 @@ export function BlocketLeagueLab({
             editable={editable}
             className={styles.sectionCopy}
           />
+          <div className={styles.conclusionCredits}>
+            <a href="https://modal.com" target="_blank" rel="noreferrer">
+              for $10 of compute on Modal
+            </a>
+            <span className={styles.qedMark} role="img" aria-label="Q.E.D." />
+            <a href="https://modal.com" target="_blank" rel="noreferrer">
+              Built on Modal
+            </a>
+          </div>
         </div>
       </section>
 
